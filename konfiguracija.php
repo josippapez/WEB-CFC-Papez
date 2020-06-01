da789b15aa9bf0e6be9d90e171b96bc2e3774df5
Josip Papez <papezjosip@gmail.com>
2020-06-01 16:05:51 +0200

    switched to external db

--- konfiguracija.php
+++ konfiguracija.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-$mysqlHost="localhost";
-$mysqlBaza="baza";
-$mysqlKorisnik="root";
-$mysqlLozinka="";
+$mysqlHost="eu-cdbr-west-03.cleardb.net";
+$mysqlBaza="heroku_4a760cf305f0dfa";
+$mysqlKorisnik="b8812483718ed0";
+$mysqlLozinka="3286566e";
 
 $veza = new PDO("mysql:host=" . $mysqlHost. ";dbname=" . $mysqlBaza,$mysqlKorisnik,$mysqlLozinka);
 $veza->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
